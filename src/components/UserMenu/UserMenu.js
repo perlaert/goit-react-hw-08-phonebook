e78f55cc8530a12c939897966b7bb23a7416e89d
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2021-06-17 14:49:56 +0300

    adds icon

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -3,14 +3,18 @@ import defaultImg from '../../images/default-avatar.jpg';
 import authOperations from '../../redux/auth/auth-operations';
 import authSelectors from '../../redux/auth/auth-selectors';
 import style from './UserMenu.module.css';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const UserMenu = ({ avatar, email, onSingout }) => {
   return (
     <div className={style.userMenu}>
-      <img src={avatar} alt="" width="32" className={style.avatar} />
-      <span className={style.email}>{email}</span>
+      <div className={style.persInfo}>
+        <img src={avatar} alt="" width="32" className={style.avatar} />
+        <span className={style.email}>{email}</span>
+      </div>
       <button type="button" onClick={onSingout} className={style.btn}>
-        Sing out
+        {/* Sing out */}
+        <ExitToAppIcon />
       </button>
     </div>
   );
